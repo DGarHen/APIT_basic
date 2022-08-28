5a6a2af638adc13833fa6e0f41635d9afc1bf095
Juan Esteban M <jmurillo@lulobank.com>
2022-08-27 20:29:46 -0500

    Cucumber

---
+++ src/test/java/com/app/stepdefinitions/ApiRestStepDefinition.java
@@ -0,0 +1,22 @@
+package com.app.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ApiRestStepDefinition {
+
+    @Given("that a QA automation is sad for this shit is'nt working")
+    public void that_a_qa_automation_is_sad_for_this_shit_is_nt_work() {
+    }
+    @When("execute the test for last time")
+    public void execute_the_for_last_time() {
+    }
+    @When("this work")
+    public void this_work() {
+    }
+    @Then("print {string}")
+    public void print(String string) {
+        System.out.println(string);
+    }
+}
---
+++ src/test/resources/features/GetUser.feature
@@ -0,0 +1,7 @@
+Feature: Automator's life
+
+  Scenario: Initial config environment
+    Given that a QA automation is sad for this shit is'nt working
+    When execute the test for last time
+    And this work
+    Then print "I'm Happy"
